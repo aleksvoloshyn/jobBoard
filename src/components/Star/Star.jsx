623eb3b59a3f901e1a34113009b5c79d54868034
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2022-11-12 23:57:40 +0200

    stars styles fixed

--- src/components/Star/Star.jsx
+++ src/components/Star/Star.jsx
@@ -1,7 +1,7 @@
 import sprite from './../../images/sprite.svg'
 const Star = () => {
   return (
-    <svg className="w-2.5 h-2.5 mr-1px">
+    <svg className="w-2.5 h-2.5 mr-px">
       <use href={`${sprite}#star`} />
     </svg>
   )
