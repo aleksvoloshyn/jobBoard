41095b72d3e6b89b7b9a8bd916b7b66a4baa40ce
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2022-11-13 00:59:48 +0200

    tailwind config modified, isLoading added

--- src/components/Detailedjob/Detailedjob.jsx
+++ src/components/Detailedjob/Detailedjob.jsx
@@ -8,6 +8,7 @@ import sprite from './../../images/sprite.svg'
 
 const Detailedjob = () => {
   const { id } = useParams()
+  const [isLoading, setIsloading] = useState(true)
   const [details, setDetails] = useState({})
   const [description, setDescription] = useState('')
   const [responsopilities, setResponsopilities] = useState('')
@@ -43,134 +44,139 @@ const Detailedjob = () => {
         setSalary(task[0].salary.replaceAll('k', ' 000'))
         setDetails(...task)
       })
+      .finally(() => {
+        setIsloading(false)
+      })
   }, [id])
 
   return (
     <Container>
-      <div className="jobDetails pb-9 px-4 pt-6 text-left bg-[white] text-detPrText">
-        <h2 className="font-bold text-3xl leading-9 mb-3">Job Details</h2>
-        <hr className="text-hr opacity-10 mb-6" />
+      {!isLoading && (
+        <div className="jobDetails pb-9 px-4 pt-6 text-left bg-[white] text-detPrText">
+          <h2 className="font-bold text-3xl leading-9 mb-3">Job Details</h2>
+          <hr className="text-hr opacity-10 mb-6" />
 
-        {/* user Panel */}
-        <ul className="userPanel flex mb-8">
-          <li className="flex mr-9 cursor-pointer">
-            <svg className="starempty w-star h-star mr-3 ">
-              <use href={`${sprite}#starempty`} />
-            </svg>{' '}
-            <p>Save to my list</p>
-          </li>
-          <li className="flex cursor-pointer">
-            <svg className="share w-share h-share mr-2.5">
-              <use href={`${sprite}#share`} />
-            </svg>{' '}
-            <p>Share</p>
-          </li>
-        </ul>
+          {/* user Panel */}
+          <ul className="userPanel flex mb-8">
+            <li className="flex mr-9 cursor-pointer">
+              <svg className="starempty w-star h-star mr-3 ">
+                <use href={`${sprite}#starempty`} />
+              </svg>{' '}
+              <p>Save to my list</p>
+            </li>
+            <li className="flex cursor-pointer">
+              <svg className="share w-share h-share mr-2.5">
+                <use href={`${sprite}#share`} />
+              </svg>{' '}
+              <p>Share</p>
+            </li>
+          </ul>
 
-        <h3 className="text-2xl font-bold leading-3xl mb-1">
-          {details.title}{' '}
-        </h3>
-        <div className="panelInfo mb-3.5 flex justify-between items-center">
-          <p className="text-xs opacity-60 text-detSecText">
-            posted {getDaysPassed(details.createdAt)} days ago
-          </p>
-          <div className="salaryInfo flex flex-col items-end">
-            <p>Brutto, per year</p>
-            <p className="font-bold text-xl">€ {salary}</p>
+          <h3 className="text-2xl font-bold leading-3xl mb-1">
+            {details.title}{' '}
+          </h3>
+          <div className="panelInfo mb-3.5 flex justify-between items-center">
+            <p className="text-xs opacity-60 text-detSecText">
+              posted {getDaysPassed(details.createdAt)} days ago
+            </p>
+            <div className="salaryInfo flex flex-col items-end">
+              <p>Brutto, per year</p>
+              <p className="font-bold text-xl">€ {salary}</p>
+            </div>
           </div>
-        </div>
-        <div className="description text-detSecText">
-          <p className="text-lg leading-6 mb-11">{description}</p>
+          <div className="description text-detSecText">
+            <p className="text-lg leading-6 mb-11">{description}</p>
 
-          <h4 className="font-bold text-xl mb-3">Responsopilities: </h4>
-          <p className="text-lg leading-6 mb-11">{responsopilities}</p>
-          <h4 className="font-bold text-xl mb-3">Compensation & Benefits:</h4>
+            <h4 className="font-bold text-xl mb-3">Responsopilities: </h4>
+            <p className="text-lg leading-6 mb-11">{responsopilities}</p>
+            <h4 className="font-bold text-xl mb-3">Compensation & Benefits:</h4>
 
-          <ul className="text-lg leading-6 mb-11">
-            {compensation.map((benefit, ind) => {
+            <ul className="text-lg leading-6 mb-11">
+              {compensation.map((benefit, ind) => {
+                return (
+                  <li key={ind} className=" ml-6 list-square">
+                    {benefit}
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+          <button className="text-[white] mb-32 text-xs bg-applbutton h-button m-auto w-32 flex justify-center items-center rounded-lg">
+            {' '}
+            APPLY NOW
+          </button>
+          <h3 className="text-3xl mb-2.5 font-bold">Attached images</h3>
+          <hr className=" mb-2.5 text-hr opacity-10" />
+          <ul className="flex justify-between gap-2 mb-16 ">
+            {pictures.map((pic, ind) => {
               return (
-                <li key={ind} className=" ml-6 list-square">
+                <li key={ind}>
+                  {' '}
+                  <img
+                    src={pic}
+                    alt="gob detail"
+                    className="rounded-[8%] drop-shadow-mobile"
+                  />
+                </li>
+              )
+            })}
+          </ul>
+          <h3 className="text-3xl mb-2.5 font-bold">Additional info</h3>
+          <hr className="text-hr opacity-10 mb-6" />
+          <h4 className="text-xl mb-2.5">Employment type</h4>
+          <ul className="mb-5 flex gap-2">
+            {employmenttype.map((type, ind) => {
+              return (
+                <li
+                  key={ind}
+                  className="w-32 h-12 border-solid border border-butEmplTypeBrdr rounded-lg bg-butEmplType text-butEmplTypeTxt flex justify-center items-center font-bold"
+                >
+                  {type}
+                </li>
+              )
+            })}
+          </ul>
+
+          <h4 className="text-xl mb-2.5 ">Benefits</h4>
+          <ul className="mb-16 flex justify-start">
+            {benefits.map((benefit, ind) => {
+              return (
+                <li
+                  key={ind}
+                  className=" mr-1.5 px-3.5 h-14 border-solid border rounded-lg bg-butBeniBg border-butBeniBrdr text-butBeniTxt flex justify-center items-center font-bold"
+                >
                   {benefit}
                 </li>
               )
             })}
           </ul>
-        </div>
-        <button className="text-[white] mb-32 text-xs bg-applbutton h-button m-auto w-32 flex justify-center items-center rounded-lg">
-          {' '}
-          APPLY NOW
-        </button>
-        <h3 className="text-3xl mb-2.5 font-bold">Attached images</h3>
-        <hr className=" mb-2.5 text-hr opacity-10" />
-        <ul className="flex justify-between gap-2 mb-16 ">
-          {pictures.map((pic, ind) => {
-            return (
-              <li key={ind}>
-                {' '}
-                <img
-                  src={pic}
-                  alt="gob detail"
-                  className="rounded-[8%] drop-shadow-mobile"
-                />
-              </li>
-            )
-          })}
-        </ul>
-        <h3 className="text-3xl mb-2.5 font-bold">Additional info</h3>
-        <hr className="text-hr opacity-10 mb-6" />
-        <h4 className="text-xl mb-2.5">Employment type</h4>
-        <ul className="mb-5 flex gap-2">
-          {employmenttype.map((type, ind) => {
-            return (
-              <li
-                key={ind}
-                className="w-32 h-12 border-solid border border-butEmplTypeBrdr rounded-lg bg-butEmplType text-butEmplTypeTxt flex justify-center items-center font-bold"
-              >
-                {type}
-              </li>
-            )
-          })}
-        </ul>
+          <h4 className="text-3xl mb-2.5 font-bold">Contacts</h4>
+          <hr className="text-hr opacity-10 mb-5" />
+          {/* <img src={contactsMob} alt="contacts map" /> */}
+          <div className="w-mapMobile   m-auto ">
+            <div className="contacts h-contMobile text-geolocation bg-geolocationBg pt-8 pl-16 rounded-t-xl">
+              <p className="font-bold"> Department name.</p>{' '}
+              <p className="font-bold mb-4"> {details.name}</p>
+              <span className="address flex mb-1.5">
+                <svg className="w-location h-location mr-2.5">
+                  <use href={`${sprite}#location`} />
+                </svg>
+                <p>{details.address}</p>
+              </span>
+              <p className="mr-2.5">{details.phone}</p>
+              <p>{details.email}</p>
+            </div>
 
-        <h4 className="text-xl mb-2.5 ">Benefits</h4>
-        <ul className="mb-16 flex justify-start">
-          {benefits.map((benefit, ind) => {
-            return (
-              <li
-                key={ind}
-                className=" mr-1.5 px-3.5 h-14 border-solid border rounded-lg bg-butBeniBg border-butBeniBrdr text-butBeniTxt flex justify-center items-center font-bold"
-              >
-                {benefit}
-              </li>
-            )
-          })}
-        </ul>
-        <h4 className="text-3xl mb-2.5 font-bold">Contacts</h4>
-        <hr className="text-hr opacity-10 mb-5" />
-        {/* <img src={contactsMob} alt="contacts map" /> */}
-        <div className="w-mapMobile   m-auto ">
-          <div className="contacts h-contMobile text-geolocation bg-geolocationBg pt-8 pl-16 rounded-t-xl">
-            <p className="font-bold"> Department name.</p>{' '}
-            <p className="font-bold mb-4"> {details.name}</p>
-            <span className="address flex mb-1.5">
-              <svg className="w-location h-location mr-2.5">
-                <use href={`${sprite}#location`} />
-              </svg>
-              <p>{details.address}</p>
-            </span>
-            <p className="mr-2.5">{details.phone}</p>
-            <p>{details.email}</p>
-          </div>
-
-          <div className="bg-geolocationBg h-googleMobile rounded-b-xl">
-            <img
-              src={`https://maps.googleapis.com/maps/api/staticmap?center=${geolocation.lat},${geolocation.long}&zoom=12&size=372x230&maptype=roadmap&key=AIzaSyASyHvDC7SCL7RencmuMJd68Zfz_InZYTw`}
-              alt="location"
-              className="opacity-20"
-            />
+            <div className="bg-geolocationBg h-googleMobile rounded-b-xl">
+              <img
+                src={`https://maps.googleapis.com/maps/api/staticmap?center=${geolocation.lat},${geolocation.long}&zoom=12&size=372x230&maptype=roadmap&key=AIzaSyASyHvDC7SCL7RencmuMJd68Zfz_InZYTw`}
+                alt="location"
+                className="opacity-20"
+              />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </Container>
   )
 }
