51ee8ee3aa51ee60d353373c419ff712bdfb2072
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2023-03-22 11:15:58 +0200

    google maps fixed

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -17,11 +17,17 @@ const Contacts = ({ name, address, email, phone, lat, long, widthClass }) => {
       </div>
 
       <div className="bg-geolocationBg h-googleMobile rounded-b-xl">
-        <img
+        {/* <img
           src={`https://maps.googleapis.com/maps/api/staticmap?center=${lat},${long}&zoom=12&size=372x230&maptype=roadmap&key=AIzaSyASyHvDC7SCL7RencmuMJd68Zfz_InZYTw`}
           alt="location"
           className="opacity-40 "
-        />
+        /> */}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d176360.46943550903!2d31.290539!3d51.49578!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46d5489287a50f97%3A0x16d9de5c19e6a614!2z0KfQtdGA0L3QuNCz0L7Qsiwg0KfQtdGA0L3QuNCz0L7QstGB0LrQsNGPINC-0LHQu9Cw0YHRgtGMLCAxNDAwMA!5e1!3m2!1sru!2sua!4v1679476333777!5m2!1sru!2sua"
+          width="372"
+          height="300"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>{' '}
       </div>
     </div>
   )
